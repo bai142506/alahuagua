434af58bf34017da58b586dc5360c4fe11bb4e33
bai142506 <50259208+bai142506@users.noreply.github.com>
2019-06-16 01:49:16 +0800

    Add files via upload

---
+++ youtube/playList.js
@@ -0,0 +1,14 @@
+console.log(12345678);
+//Youtube 影片 ID
+var playList=[
+    "ILoTP6R1_0c",
+    "zu8hpj883H4",
+    "bvtKensco6E"
+];
+
+//播放起迄秒數
+var playTime=[
+    [0,20],
+    [0,20],
+    [0,20]
+];
